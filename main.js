795da10f80a3d2e39fb8db26e83b6fec158064fc
Warren Ambuga <ambushwarren@gmail.com>
2026-02-16 12:18:31 +0300

    Updated boiler plate code, styles and javascript functionality for my To-do App

--- main.js
+++ main.js
@@ -4,29 +4,41 @@ let taskInput = document.getElementById('input-task');
 const taskContainer = document.getElementById('task-container');  
 
 addTaskBtn.addEventListener('click', function() {
-    let task = document.creatElement('div');
-    task.classList.add('task')
+    let task = document.createElement('div');
+    task.classList.add('task');
 
     let li = document.createElement('li');
     li.innerText = `${taskInput.value}`;
     task.appendChild(li);
 
     let checkBtn = document.createElement('button');
-    checkBtn.innerHtml = '<i class="fa-solid fa-check"';
-    checkBtn.classList.add('checkTask');
+    checkBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+    checkBtn.classList.add('check-task');
     task.appendChild(checkBtn);
 
     let deleteBtn = document.createElement('button');
-    deleteBtn.innerHtml = '<i class="fa-solid fa-trash"';
-    checkBtn.classList.add('deleteTask');
+    deleteBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    deleteBtn.classList.add('delete-task');
     task.appendChild(deleteBtn);
 
 
-    if(taskInput==="") {
+    if(taskInput.value==="") {
         alert("Please Enter Task");
     }else{
         taskContainer.appendChild(task)
     }
 
-    inputTask.value = "";
+    taskInput.value = "";
+
+
+    checkBtn.addEventListener('click', function() {
+       
+        checkBtn.parentElement.style.textDecoration = 'line-through'
+    })
+
+     deleteBtn.addEventListener('click', function(e) {
+       
+        let target = e.target;
+        target.parentElement.parentElement.remove()
+    })
 })
